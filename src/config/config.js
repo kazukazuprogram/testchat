e2d746ff61ce0beee1aa284d2ebf658b40818530
kazukazuprogram <A>
2020-10-25 18:57:29 +0900

    OK

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApvY9RXrTLfQhM-KCaf2ne2gWx-taknOY",
+  authDomain: "testchat-165e6.firebaseapp.com",
+  databaseURL: "https://testchat-165e6.firebaseio.com",
+  projectId: "testchat-165e6",
+  storageBucket: "testchat-165e6.appspot.com",
+  messagingSenderId: "62666845984",
+  appId: "1:62666845984:web:18540fe253a6aed73a3145",
+  measurementId: "G-BHSMP82CNH"
+};
